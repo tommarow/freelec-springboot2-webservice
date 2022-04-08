fca75206ed1ea1ede1b17d4d2364a6da0579f525
KwangHo Cheong <tommarow@naver.com>
2022-04-09 00:20:03 +0900

    4장 완료

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -7,6 +7,9 @@ var main = {
         $('#btn-update').on('click', function() {
             _this.update();
         });
+        $('#btn-delete').on('click', function() {
+            _this.delete();
+        });
     },
     save: function () {
         var data = {
@@ -29,26 +32,41 @@ var main = {
         })
     },
     update: function () {
-            var data = {
-                title: $('#title').val(),
-                content: $('#content').val()
-            };
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
 
-            var id = $('#id').val();
+        var id = $('#id').val();
 
-            $.ajax({
-                type: 'PUT',
-                url: '/api/v1/posts/' + id,
-                dataType: 'json',
-                contentType: 'application/json; charset=utf-8',
-                data: JSON.stringify(data)
-            }).done(function() {
-                alert('글이 수정되었습니다.');
-                window.location.href='/';
-            }).fail(function(error) {
-                alert(JSON.stringify(error));
-            })
-        }
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정되었습니다.');
+            window.location.href='/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        })
+    },
+     delete: function () {
+         var id = $('#id').val();
+
+         $.ajax({
+             type: 'DELETE',
+             url: '/api/v1/posts/' + id,
+             dataType: 'json',
+             contentType: 'application/json; charset=utf-8'
+         }).done(function() {
+             alert('글이 삭제되었습니다.');
+             window.location.href='/';
+         }).fail(function(error) {
+             alert(JSON.stringify(error));
+         })
+     }
 };
 
 main.init();
