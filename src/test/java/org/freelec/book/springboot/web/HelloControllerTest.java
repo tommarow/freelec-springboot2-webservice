004b462873eee2968b17a160e50844faed17c209
KwangHo Cheong <tommarow@naver.com>
2022-04-26 17:23:19 +0900

    소스 변경 - https://jojoldu.tistory.com/539 참조

--- src/test/java/org/freelec/book/springboot/web/HelloControllerTest.java
+++ src/test/java/org/freelec/book/springboot/web/HelloControllerTest.java
@@ -1,14 +1,14 @@
 package org.freelec.book.springboot.web;
 
 import org.freelec.book.springboot.config.auth.SecurityConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -16,7 +16,7 @@ import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class,
         excludeFilters = {
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
--- src/test/java/org/freelec/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/org/freelec/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,6 +1,6 @@
 package org.freelec.book.springboot.web.dto;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloResponseDtoTest {
