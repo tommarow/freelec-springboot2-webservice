1dda1e5c3f1cdcfdd587275e485e238073e23fb0
KwangHo Cheong <tommarow@naver.com>
2022-04-27 19:40:16 +0900

    10.4 무중단 배포 테스트 완료 - 빌드 실패로 수정2

--- scripts/start.sh
+++ scripts/start.sh
@@ -33,6 +33,6 @@ echo "nohup java -jar " \
      "$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &"
 
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=$IDLE_PROFILE \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
